c83a29cd536701264fc1b4f0d777b5ccd458c3d2
Dima Komisarov <komissarov854@gmail.com>
2025-06-28 22:21:47 +0300

    feat: add login page

--- src/modules/auth/components/Card/Card.tsx
+++ src/modules/auth/components/Card/Card.tsx
@@ -9,8 +9,7 @@ export const Card = styled(MuiCard)(({ theme }) => ({
   // gap: theme.spacing(2),
   padding: 30,
   gap: 20,
-  boxShadow:
-    `hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px,
+  boxShadow: `hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px,
     hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px`,
   // [theme.breakpoints.up('sm')]: {
   //   width: '450px',
@@ -19,6 +18,5 @@ export const Card = styled(MuiCard)(({ theme }) => ({
   //   boxShadow:
   //     'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
   // }),
-  ...theme
+  ...theme,
 }));
-
